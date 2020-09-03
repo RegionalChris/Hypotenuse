a8ce59b345727c9fd93d23db68bfc98fb97a7ffc
chris <chris@LAPTOP-112SM9N4>
2020-09-03 01:16:15 -0400

    Here is my equation to find the hypotenuse

--- Hypotenuse/Program.cs
+++ Hypotenuse/Program.cs
@@ -6,7 +6,16 @@ namespace Hypotenuse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double side1;
+            double side2;
+            double hypotenuse;
+            Console.Write("Side 1 : ");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Side 2 : ");
+            side2 = Convert.ToDouble(Console.ReadLine());
+            hypotenuse = Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2));
+            Console.WriteLine("Hypotenuse : " + hypotenuse);
+            Console.ReadKey();
         }
     }
 }
