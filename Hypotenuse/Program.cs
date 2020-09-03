59b565b3be6af34b59c6185ca30c93ace91f040b
chris <chris@LAPTOP-112SM9N4>
2020-09-03 01:12:40 -0400

    Add project files.

---
+++ Hypotenuse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
